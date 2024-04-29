29ee1b95873deee1a814655cd26a4e67bdbf1bd4
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-04-29 17:10:11 +0200

    Added: M2 - random + even/odd

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,5 @@
+// FUNZIONE PALINDROMA
+
 function palindroma(parola){
     
     const reverseWord = [];
@@ -12,11 +14,38 @@ function palindroma(parola){
     }
     
     if(parola == parolaDaComparare){
-        console.log("La parola inserita è palindroma!");
         document.getElementById('pali').innerHTML="La parola inserita \" " + parola + "\" è palindroma!";
     }
     else{
-        console.log("La parola inserita non è palindroma!");
         document.getElementById('pali').innerHTML="La parola inserita \" " + parola + "\" non è palindroma!";
+    }
+}
+
+
+// FUNZIONE ESTRAZIONE NUMERO RANDOMICO TRA 1 E 5
+
+function randomNumber(){
+    let random = Math.floor(Math.random() * 5) + 1;
+    console.log("Sono nel randomNumber e il numero random è " + random);
+    return random;
+}
+
+
+// FUNZIONE PARI/DISPARI
+
+function pariDispari(numero, scelta){
+
+    if(numero % 2 ==0 && scelta == "pari"){
+        document.getElementById('pardisp').innerHTML="Il risultato è " + numero + " ed è pari! L'utente ha vinto!";
+    }
+    else if(numero % 2 ==0 && scelta !="pari"){
+        document.getElementById('pardisp').innerHTML="Il risultato è " + numero + " ed è pari! L'utente ha perso!";
+    }
+   
+    else if(numero % 2 != 0 && scelta != "pari"){
+        document.getElementById('pardisp').innerHTML="Il risultato è " + numero + " ed è dispari! L'utente ha vinto!";
+    }
+    else if(numero % 2 != 0 && scelta == "pari"){
+        document.getElementById('pardisp').innerHTML="Il risultato è " + numero + " ed è dispari! L'utente ha perso!";
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 let parola = prompt("Inserisci una parola: ");
 palindroma(parola);
+
+let sceltaUtente = prompt("Scegli pari o dispari");
+let numeroUtente = parseInt(prompt("Inserisci un numero tra 1 e 5"));
+let numeroRandom = randomNumber();
+
+const somma = parseInt(numeroUtente + numeroRandom);
+pariDispari(somma);
